68ae272deff0f6c39e897014e1952dcfce7d2b6c
Timglobal <110417897+Timglobal@users.noreply.github.com>
2025-08-29 17:06:31 +0000

    upate connection

--- sentiments-api/server.js
+++ sentiments-api/server.js
@@ -75,7 +75,7 @@ app.use((req, res, next) => {
 const PORT = process.env.PORT || 8000
 
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true,authSource:"admin" })
   .then(async () => {
     console.log('✅ MongoDB connected')
     
